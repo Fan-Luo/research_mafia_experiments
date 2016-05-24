5d074b2ef1dd0be61bc36002e849e1234c0ffefb
Fan-Luo <luofan813@gmail.com>
2016-05-24 11:59:23 -0400

    insight

--- copy_files_to_drectories.sh
+++ copy_files_to_drectories.sh
@@ -5,11 +5,12 @@ listname="single_swl_list.txt"
 
 cat $listname | while read dir_name
 do
+	cp -r ./apps.txt ./$dir_name
+	cp -r ./apps2.txt ./$dir_name
+	cp -r ./apps3.txt ./$dir_name
+	cp -r ./apps4.txt ./$dir_name
 	cp -r ./script_base_singleapp.pbs ./$dir_name
 	cp -r ./script_base_workloads.pbs  ./$dir_name
-	cp -r ./setup_singleapp.sh  ./$dir_name
-	cp -r ./setup_workload.sh  ./$dir_name
-
 done
 
 
